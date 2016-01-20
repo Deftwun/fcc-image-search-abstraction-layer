fbe7a9f6ecf27b8caaac4af78ded1a4bc2b0832f
Jeff Wakeman <deftwun@gmail.com>
2016-01-20 22:10:42 +0000

    start work on routes

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ var port = process.env.PORT || 8080;
 
 app.use("/",express.static("client"));
 
+app.get("/:query",function(req,res){
+
+});
+
 app.listen(port, function () {
 console.log('url-shortener running on port ' + port);
 });
