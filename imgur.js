73008fa152dcf2e45fb57a0792cd99abb1588a01
Jeff Wakeman <deftwun@gmail.com>
2016-01-23 14:16:25 +0000

    Imgur search abstracted

--- imgur.js
+++ imgur.js
@@ -1,43 +1,33 @@
 "use strict";
 
-module.exports = search;
+require("./env.js");
 
-function search(searchString,callback,opt){
-  
-  if (!opt){
-    opt = {
-      time:"all",
-      sort:"top",
-      page:"0",
-    }
-  }
-  
-  else {
+module.exports = {
+  search : function (searchString,callback,opt){
+    
+    if (!opt) opt={};
     opt.time = opt.time | "all";
     opt.sort = opt.sort | "top";
     opt.page = opt.page | "0";
+    
+    var https = require("https");
+    
+    var options = {
+      host: 'api.imgur.com',
+      path: ["3/gallery/search",opt.sort,opt.time,opt.page+"?="+searchString].join("/"),
+      headers: {"Authorization" : "Client-ID " + process.env.CLIENT_ID}
+    };  
+    
+    var req = https.request(options, function(response) {
+      var str = '';
+      response.on('data', function (chunk) {
+        str += chunk;
+      });
+      response.on('end', function () {
+        callback(str);
+      });
+    });
+    
+    req.end();  
   }
-  
-  require("./env.js");
-  
-  var https = require("https");
-  
-  var options = {
-    host: 'api.imgur.com',
-    path: ["3/gallery/search",opt.sort,opt.time,opt.page+"?="+searchString].join("/"),
-    headers: {"Authorization" : "Client-ID " + process.env.CLIENT_ID}
-  };  
-  
-  var callback = function(response) {
-    var str = '';
-    response.on('data', function (chunk) {
-      str += chunk;
-    });
-    response.on('end', function () {
-      callback(str);
-    });
-  };
-  
-  var req = https.request(options, callback);
-  req.end();  
 }
--- server.js
+++ server.js
@@ -4,16 +4,17 @@ var express = require("express"),
     app = express();
 
 var port = process.env.PORT || 8080;
-    
-//app.use("/",express.static("client"));
 
 app.use("/",express.static("client"));
 
 app.get("/search/:str",function(req,res){
   var searchString = req.params.str,
       page = req.query.offset;
-      
-  console.log(searchString + " : " + page);
+  
+  var imgur = require("./imgur.js");
+  imgur.search("what",function(result){
+    res.end(result);
+  });
 });
 
 app.listen(port, function () {
